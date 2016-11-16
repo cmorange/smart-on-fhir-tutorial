e8106e3813fc860a3901bac4651559c3916585e4
cmorange <chris.orange@vcuhealth.org>
2016-11-16 16:05:40 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -145,7 +145,7 @@
   }
 
   window.drawVisualization = function(p) { 
-    alert(p);
+    alert(JSON.stringify(p))
     $('#holder').show();
     $('#loading').hide();
     $('#fname').html(p.fname);
